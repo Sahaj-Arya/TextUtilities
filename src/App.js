98d3b1ede66c69706ce572e87fb961739bdc76b1
Sahaj-Arya <sahajarya10@gmail.com>
2022-01-15 20:49:38 +0530

    part2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            TextUtil
+            Text Util
           </a>
           <button
             className="navbar-toggler"
